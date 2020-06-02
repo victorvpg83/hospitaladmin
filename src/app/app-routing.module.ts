4b3517b56e0379637c8db1751b124598bed67772
Victor <victor@victor.com>
2020-06-02 13:58:01 +0200

    Modulos modificados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
 import { Page404Component } from './shared/page404/page404.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graphs1', component: Graphs1Component },
-      { path: '', pathMatch: 'full', redirectTo: '/dashboard' },
-
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: Page404Component }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// modules
+import { PagesModule } from './pages/pages.module';
+
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { Page404Component } from './shared/page404/page404.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    Page404Component,
-    DashboardComponent,
-    ProgressComponent,
-    Graphs1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    PagesModule,
+    AppRoutingModule // import principal routes last
   ],
   providers: [],
   bootstrap: [AppComponent]
